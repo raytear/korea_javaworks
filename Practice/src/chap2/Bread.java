5b39d5d5bd01eb38f258d1b1c850831c5582b0d9
raytear <younji.mok@gmail.com>
2025-03-29 14:52:41 +0900

    while

---
+++ Practice/src/chap2/Bread.java
@@ -0,0 +1,20 @@
+package chap2;
+
+public class Bread {
+
+	public static void main(String[] args) {
+		// 빵 25개를 4명이 나눠 가질 경우 각자의 몫과 남은 빵의 개수를 구하시오.
+		
+		int bread = 25;
+		int person = 4;
+		int share, remainder;
+		
+		share = bread / person; //몫
+		remainder = bread % person; //나머지
+		
+		System.out.println("각자의 몫: " + share);
+		System.out.println("남은 빵의 개수: " + remainder);
+
+	}
+
+}
